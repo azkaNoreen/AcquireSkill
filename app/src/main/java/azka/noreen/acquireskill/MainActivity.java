a1d2945017915e25e353531971130e958cd04577
Azka Noreen <azka.noreen6@gmail.com>
2023-01-07 16:17:10 +0500

    onPostExecute

--- app/src/main/java/azka/noreen/acquireskill/MainActivity.java
+++ app/src/main/java/azka/noreen/acquireskill/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -19,15 +21,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 //                while(true);
-               new MyAsyncTask().execute();
+               new MyAsyncTask().execute("Azka");
             }
         });
-
     }
     private class MyAsyncTask extends AsyncTask<String,Void,String> {
         @Override
         protected String doInBackground(String... strings) {
-            while(true){}
+            String asd=strings[0];
+            Log.d("StringVal", asd);
+            return asd;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
         }
     }
+
+
 }
